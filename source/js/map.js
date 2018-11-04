af05890437f840e17f69350c65397732775136d0
eresha1 <eresha39@gmail.com>
2018-11-04 16:35:52 +0700

    вставлены картинки с pin, поправки в коде map.js

--- source/js/map.js
+++ source/js/map.js
@@ -1,33 +1,25 @@
   ymaps.ready(init);
-    function init(){
-        var myMap = new ymaps.Map("map", {
-            center: [59.93863106417265,30.3230545],
-            zoom: 17
-        });
+  function init(){
+    var myMap = new ymaps.Map("map", {
+      center: [59.93863106417265,30.3230545],
+      zoom: 17,
 
-        var myPlacemark = new ymaps.Placemark([59.93863106417265,30.3230545], {}, {
-    iconLayout: "default#image",
-    iconImageHref: "../img/icon-map-pin.svg",
-    iconImageSize: [66, 101],
-    iconImageOffset: [-40, -90],
-});
+      controls: []
+    });
+
+    var myPlacemark = new ymaps.Placemark([59.93863106417265,30.3230545], {}, {
+      iconLayout: "default#image",
+      iconImageHref: "../img/icon-map-pin.svg",
+      iconImageSize: [66, 101],
+      iconImageOffset: [-40, -90],
+    });
 
     myMap.geoObjects.add(myPlacemark);
+    myMap.controls.add('zoomControl');
 
     myMap.behaviors.disable([
-    "drag",
-    "dblClickZoom",
-    "rightMouseButtonMagnifier",
-    "ruler",
-    "routeEditor",
-    "scrollZoom"
-  ]);
-  myMap.controls
-    .remove("searchControl")
-    .remove("routeButtonControl")
-    .remove("trafficControl")
-    .remove("typeSelector")
-    .remove("fullscreenControl")
-    .remove("rulerControl");
-
-    }
+      "rightMouseButtonMagnifier",
+      "ruler",
+      "routeEditor",
+      ]);
+  }
