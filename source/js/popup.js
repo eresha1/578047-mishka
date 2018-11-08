da00ba42c0a47216c7d15c8b20ae84ef0dc26066
eresha1 <eresha39@gmail.com>
2018-11-08 22:57:21 +0700

    исправления

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,13 +1,21 @@
-var link = document.querySelectorAll(".card__buy--cart");
-var popup = document.querySelector(".modal");
+var link = document.querySelectorAll(".card__buy-cart");
+var popup = document.querySelector(".modal__wrapper");
 var overlay = document.querySelector(".modal__overlay");
+var orderButton = document.querySelector(".goods-week__order");
 
+if (orderButton) {
+    orderButton.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal__wrapper--show");
+      overlay.classList.add("modal__overlay--show");
+    });
+  }
 
 if (link !== null) {
   for (var i = 0; i < link.length; i++) {
     link[i].addEventListener("click", function (evt) {
       evt.preventDefault();
-      popup.classList.add("modal--show");
+      popup.classList.add("modal__wrapper--show");
       overlay.classList.add("modal__overlay--show");
     });
   }
@@ -16,8 +24,8 @@ if (link !== null) {
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("modal--show")) {
-      popup.classList.remove("modal--show");
+    if (popup.classList.contains("modal__wrapper--show")) {
+      popup.classList.remove("modal__wrapper--show");
       overlay.classList.remove("modal__overlay--show");
     }
   }
@@ -25,6 +33,6 @@ window.addEventListener("keydown", function (evt) {
 
 overlay.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal--show");
+  popup.classList.remove("modal__wrapper--show");
   overlay.classList.remove("modal__overlay--show");
 });
